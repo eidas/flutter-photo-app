5118b3ba5f1975a505e96387de209ab76d03736a
eidas <eidagm@gmail.com>
2025-03-29 17:13:52 +0900

    Add camera data source implementation

---
+++ lib/data/datasources/camera_data_source.dart
@@ -0,0 +1,52 @@
+import 'dart:typed_data';
+import 'package:camera/camera.dart';
+
+/// カメラ操作を抽象化するデータソースインターフェース
+abstract class CameraDataSource {
+  /// カメラを初期化する
+  Future<void> initialize();
+  
+  /// カメラのプレビューストリームを取得する
+  Stream<CameraImage> getPreviewStream();
+  
+  /// 写真を撮影し、バイナリデータとして返す
+  Future<Uint8List> capturePhoto();
+  
+  /// カメラリソースを解放する
+  Future<void> dispose();
+}
+
+/// カメラデータソースの実装クラス
+class CameraDataSourceImpl implements CameraDataSource {
+  late CameraController _cameraController;
+  final CameraDescription _camera;
+  
+  CameraDataSourceImpl(this._camera);
+  
+  @override
+  Future<void> initialize() async {
+    _cameraController = CameraController(
+      _camera,
+      ResolutionPreset.high,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg, // 明示的な形式指定
+    );
+    await _cameraController.initialize();
+  }
+  
+  @override
+  Stream<CameraImage> getPreviewStream() {
+    return _cameraController.startImageStream();
+  }
+  
+  @override
+  Future<Uint8List> capturePhoto() async {
+    final XFile file = await _cameraController.takePicture();
+    return await file.readAsBytes();
+  }
+  
+  @override
+  Future<void> dispose() async {
+    await _cameraController.dispose();
+  }
+}
